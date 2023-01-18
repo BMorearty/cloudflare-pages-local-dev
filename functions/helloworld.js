99992166598f4093fffeb314157b0c1dbd69bf9c
Brian Morearty <brian@morearty.org>
2023-01-17 16:58:30 -0800

    test

--- functions/helloworld.js
+++ functions/helloworld.js
@@ -1,4 +1,5 @@
 export async function onRequest(context) {
-  const name = await context.env.TEST.get('name', 'Brian');
+  // const name = await context.env.TEST.get('name', 'Brian');
+  const name = JSON.stringify(Object.keys(context.env));
   return new Response(`hello, ${name}`);
 }
